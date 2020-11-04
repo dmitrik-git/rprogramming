6abde7aa2a21aac2fe13c135b7647d33a49b0ab9
Dmitri K <dmitri.kovagin@gmail.com>
2020-11-04 13:11:46 +0200

    Quiz finished!

--- week1quiz.R
+++ week1quiz.R
@@ -2,7 +2,19 @@ data <-read.csv("hw1_data.csv")
 theCols<- c("Ozone","Temp","Solar.R")
 #data[,theCols]
 
-a=data[data$Ozone > 41 & data$Temp > 67,"Solar.R"]
-mean(a)
+##Question 18
+a=data[data$Ozone > 31 & data$Temp > 90,"Solar.R"]
+a1=complete.cases(a)
+mean(a[a1])
 
+##Question 19
+b=data[data$Month == 6,"Temp"]
+##b1=complete.cases(b)
+##mean(b[b1])
+mean(b)
+
+##Question 20
+c=data[data$Month == 5,"Ozone"]
+c1=complete.cases(c)
+max(c[c1])
 
