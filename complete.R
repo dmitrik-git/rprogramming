0f0512451376e06965d5cf4c4cb939011cb29ec9
Dmitri K <dmitri.kovagin@gmail.com>
2020-11-14 19:22:58 +0200

    finally

--- complete.R
+++ complete.R
@@ -1,35 +1,22 @@
 complete <- function(directory, id = 1:332) {
-  ##data <- data.frame(id, nobs)
   wd <- getwd() 
   setwd(paste(wd,directory,sep = "/"))
   files <- list.files(full.names = FALSE)
-  for (i in id) {
+  v <- numeric(length(id))
+  k <- 1
+    for (i in id) {
     filedata <- read.csv(files[i])
-    ##data[i,id, nobs] <- c(files[i], sum(complete.cases(filedata)))
-    data[i] <- data.frame(id=max(filedata["ID"]), nobs=sum(complete.cases(filedata)))
-  }
+    row.of.df <- sum(complete.cases(filedata))
+    v[k] <- row.of.df
+    k <- k+1
+    }
     setwd (wd)
+    data <- data.frame (id, nobs = v)
     data
 }
 
 complete("specdata", 1)
 complete("specdata", c(2, 4, 8, 10, 12))
+complete("specdata", 30:25)
+complete("specdata", 3)
 
-data <- data.frame()
-data[1,1] <- c("2")
-data[1,2] <- c("5")
-
-filedata <- read.csv("072.csv")
-c("72", sum(complete.cases(filedata)))
-c
-?data.frame
-
-a=complete.cases(data)
-a
-data[a]
-getwd()
-setwd("C:/Users/dmitri.kovagin/Documents/RStudio/rprogramming")
-setwd("C:/Users/dmitri.kovagin/Documents/RStudio/rprogramming/specdata")
-data
-sum(!a)
-data[1,1:2] <- c(3, 33)
