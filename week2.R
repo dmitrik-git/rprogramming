fd20a4f621d54cf662ea7e78e49f164ac76c851f
Dmitri K <dmitri.kovagin@gmail.com>
2020-11-14 13:25:14 +0200

    week2

--- week2.R
+++ week2.R
@@ -20,3 +20,26 @@ columnmean <- function(x, removeNA = TRUE) {
   }
   means
 }
+
+f <- function(x) {
+  g <- function(y) {
+    y + z
+  }
+  z <- 4
+  x + g(x)
+}
+z <- 10
+f(3)
+
+x <- 5
+y <- if(x < 3) {
+  NA
+} else {
+  10
+}
+y
+
+x <- 1:10
+if(x > 5) {
+  x <- 0
+}
